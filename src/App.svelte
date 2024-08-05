7637fa411c152e35365f0d92324c11d8a077e333
Felix Zett <FelixZett@users.noreply.github.com>
2024-08-05 11:42:59 +0000

    Add barebones svelte app

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Navbar from './lib/Navbar.svelte';
+</script>
+
+<head></head>
+<main>
+  <div class="container mx-auto">
+    <Navbar/>
+    <button class="btn">Hello</button>
+    <h1>Timberborn Calculator</h1>
+    <h2>Update 6 Experimental</h2>
+  </div>
+</main>
